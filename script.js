2dc7cf25c479190b71ea66e01a7cce086409fab4
Rahul Singh <rahulsinghrajpoot9997@gmail.com>
2025-12-11 09:23:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+const buttonsSection = document.getElementById("buttons");
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  buttonsSection.appendChild(btn);
+});
+
+const stopBtn = document.createElement("button");
+stopBtn.classList.add("btn", "stop");
+stopBtn.innerText = "stop";
+
+stopBtn.addEventListener("click", stopSongs);
+
+buttonsSection.appendChild(stopBtn);
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
